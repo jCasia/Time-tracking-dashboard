1d74b2317bef4279563229f10bb8658feae2c897
jCasia <casjay70@gmail.com>
2023-06-11 23:49:02 +0100

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,5 @@
+function App() {
+  return <h1 className='text-colorBlue text-3xl font-Rubik'>Testing</h1>;
+}
+
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    extend: {},
+    colors: {
+      colorBlue: 'hsl(246, 80%, 60%)',
+    },
+    fontFamily: {
+      Rubik: ['Rubik'],
+    },
+  },
+
+  plugins: [],
+};
